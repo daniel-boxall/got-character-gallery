ac40262be2fc17ceaf354ccae26a44065be28e28
daniel-boxall <danielboxall07@gmail.com>
2023-03-09 19:55:28 +0000

    modified file structure

--- src/components/CharacterGallery.js
+++ src/components/CharacterGallery.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import characterData from '../characterData.json'
-import Character from '../components/Character'
+import characterData from '../data/characterData.json'
+import Character from './Character'
 
 const CharacterGallery = () => {
-    const characterList = characterData.map(character => <Character {...character} key={character.id} />)
+    const listOfChars = characterData.map((char, i) => <Character {...char} key={i} />)
     return (
         <div
             className='gallery-container' data-test='char-gallery'>
-            {characterList}
+            {listOfChars}
         </div>
     );
 };
